shenxiaochen/intel-cmt-cat

commit 20f07095f4e024f6fae3530f7aed6128fbe91128

lib: moved HW AMD detection of MBA to hw_cap module

lib/hw_cap.c
@@ -653,15 +653,6 @@ l2cdp_is_enabled_exit:
         return ret;
 }
 
-/**
- * @brief Discovers L2 CAT
- *
- * @param r_cap place to store L2 CAT capabilities structure
- * @param cpu CPU topology structure
- *
- * @return Operation status
- * @retval PQOS_RETVAL_OK success
- */
 int
 hw_cap_l2ca_discover(struct pqos_cap_l2ca **r_cap,
                      const struct pqos_cpuinfo *cpu)
@@ -796,6 +787,61 @@ hw_cap_mba_discover(struct pqos_cap_mba **r_cap, const struct pqos_cpuinfo *cpu)
                 free(cap);
                 return PQOS_RETVAL_RESOURCE;
         }
+
+        (*r_cap) = cap;
+        return ret;
+}
+
+int
+amd_cap_mba_discover(struct pqos_cap_mba **r_cap,
+                     const struct pqos_cpuinfo *cpu)
+{
+        struct cpuid_out res;
+        struct pqos_cap_mba *cap = NULL;
+        const unsigned sz = sizeof(*cap);
+        int ret = PQOS_RETVAL_OK;
+
+        UNUSED_PARAM(cpu);
+
+        cap = (struct pqos_cap_mba *)malloc(sz);
+        if (cap == NULL)
+                return PQOS_RETVAL_RESOURCE;
+
+        ASSERT(cap != NULL);
+
+        memset(cap, 0, sz);
+        cap->mem_size = sz;
+        cap->ctrl = -1;
+        cap->ctrl_on = 0;
+
+        /**
+         * Run CPUID.0x80000008.0 to check
+         * for MBA allocation capability (bit 6 of ebx)
+         */
+        lcpuid(0x80000008, 0x0, &res);
+        if (!(res.ebx & (1 << 6))) {
+                LOG_INFO("CPUID.0x80000008.0: MBA not supported\n");
+                free(cap);
+                return PQOS_RETVAL_RESOURCE;
+        }
+
+        /**
+         * We can go to CPUID.0x10.0 to obtain more info
+         */
+        lcpuid(0x80000020, 0x0, &res);
+        if (!(res.ebx & (1 << 1))) {
+                LOG_INFO("CPUID 0x10.0: MBA not supported!\n");
+                free(cap);
+                return PQOS_RETVAL_RESOURCE;
+        }
+
+        lcpuid(0x80000020, 1, &res);
+
+        cap->num_classes = (res.edx & 0xffff) + 1;
+
+        /* AMD does not support throttle_max and is_linear. Set it to 0 */
+        cap->throttle_max = 0;
+        cap->is_linear = 0;
 
         (*r_cap) = cap;
         return ret;

lib/cap.c
@@ -58,15 +58,11 @@
 #include <sys/stat.h> /* S_Ixxx */
 #include <pthread.h>
 
-#include "pqos.h"
-
 #include "cap.h"
 #include "os_cap.h"
 #include "hw_cap.h"
 #include "allocation.h"
 #include "monitoring.h"
-
-#include "cpu_registers.h"
 #include "cpuinfo.h"
 #include "machine.h"
 #include "types.h"
@@ -74,7 +70,6 @@
 #include "api.h"
 #include "utils.h"
 #include "resctrl.h"
-#include "perf_monitoring.h"
 
 /**
  * ---------------------------------------
@@ -250,67 +245,6 @@ _pqos_check_init(const int expect)
 }
 
 /**
- * @brief Discovers MBA feature for AMD
- *
- * @param r_cap place to store MBA capabilities structure
- *
- * @return Operation status
- * @retval PQOS_RETVAL_OK success
- * @retval PQOS_RETVAL_RESOURCE if not supported
- */
-static int
-discover_alloc_mba_amd(struct pqos_cap_mba **r_cap)
-{
-        struct cpuid_out res;
-        struct pqos_cap_mba *cap = NULL;
-        const unsigned sz = sizeof(*cap);
-        int ret = PQOS_RETVAL_OK;
-
-        cap = (struct pqos_cap_mba *)malloc(sz);
-        if (cap == NULL)
-                return PQOS_RETVAL_RESOURCE;
-
-        ASSERT(cap != NULL);
-
-        memset(cap, 0, sz);
-        cap->mem_size = sz;
-        cap->ctrl = -1;
-        cap->ctrl_on = 0;
-
-        /**
-         * Run CPUID.0x80000008.0 to check
-         * for MBA allocation capability (bit 6 of ebx)
-         */
-        lcpuid(0x80000008, 0x0, &res);
-        if (!(res.ebx & (1 << 6))) {
-                LOG_INFO("CPUID.0x80000008.0: MBA not supported\n");
-                free(cap);
-                return PQOS_RETVAL_RESOURCE;
-        }
-
-        /**
-         * We can go to CPUID.0x10.0 to obtain more info
-         */
-        lcpuid(0x80000020, 0x0, &res);
-        if (!(res.ebx & (1 << 1))) {
-                LOG_INFO("CPUID 0x10.0: MBA not supported!\n");
-                free(cap);
-                return PQOS_RETVAL_RESOURCE;
-        }
-
-        lcpuid(0x80000020, 1, &res);
-
-        cap->num_classes = (res.edx & 0xffff) + 1;
-
-        /* AMD does not support throttle_max and is_linear. Set it to 0 */
-        cap->throttle_max = 0;
-        cap->is_linear = 0;
-
-        (*r_cap) = cap;
-        return ret;
-}
-
-/**
  * @brief Runs detection of platform monitoring and allocation capabilities
  *
  * @param p_cap place to store allocated capabilities structure
@@ -425,7 +359,7 @@ discover_capabilities(struct pqos_cap **p_cap,
          */
         if (inter == PQOS_INTER_MSR) {
                 if (cpu->vendor == PQOS_VENDOR_AMD)
-                        ret = discover_alloc_mba_amd(&det_mba);
+                        ret = amd_cap_mba_discover(&det_mba, cpu);
                 else
                         ret = hw_cap_mba_discover(&det_mba, cpu);
         }

lib/hw_cap.h
@@ -88,6 +88,19 @@ int hw_cap_l2ca_discover(struct pqos_cap_l2ca **r_cap,
 int hw_cap_mba_discover(struct pqos_cap_mba **r_cap,
                         const struct pqos_cpuinfo *cpu);
 
+/**
+ * @brief Discovers MBA support for AMD
+ *
+ * @param r_cap place to store MBA capabilities structure
+ * @param cpu detected cpu topology
+ *
+ * @return Operation status
+ * @retval PQOS_RETVAL_OK success
+ * @retval PQOS_RETVAL_RESOURCE if not supported
+ */
+int amd_cap_mba_discover(struct pqos_cap_mba **r_cap,
+                         const struct pqos_cpuinfo *cpu);
+
 #ifdef __cplusplus
 }
 #endif

commit 7429877caa1825980d070d70cc2c820e26c2452f

lib: Fix double free issue in amd_cap_mba_discover()
In the call path cap_mba_discover() -> amd_cap_mba_discover(), a
double-free issue is triggered when cpuid(0x80000008.0) or
cpuid(0x80000020.0) fails. On error, amd_cap_mba_discover() calls
free(cap) and returns, but the caller, cap_mba_discover(), subsequently
calls free(cap) again.

This causes the pqos command to fail with errors on platforms that hit
this failure path (e.g., Hygon Model 4):

  $ pqos
  Selected interface: MSR
  WARN: CPUID.0x7.0: Monitoring capability not supported!
  WARN: Cache allocation not supported on model name '...'!
  free(): double free detected in tcache 2
  Aborted

Fix this by removing the redundant free(cap) call from
amd_cap_mba_discover().

Fixes: 20f07095f4e0 ("lib: moved HW AMD detection of MBA to hw_cap module")
Fixes: 72beceac50df ("lib: Introduce new function discover_alloc_mba_amd for AMD")
Signed-off-by: Xiaochen Shen <shenxiaochen@open-hieco.net>

lib/hw_cap.c
@@ -1302,7 +1302,6 @@ amd_cap_mba_discover(struct pqos_cap_mba *cap, const struct pqos_cpuinfo *cpu)
         lcpuid(0x80000008, 0x0, &res);
         if (!(res.ebx & (1 << 6))) {
                 LOG_INFO("CPUID.0x80000008.0: MBA not supported\n");
-                free(cap);
                 return PQOS_RETVAL_RESOURCE;
         }
 
@@ -1312,7 +1311,6 @@ amd_cap_mba_discover(struct pqos_cap_mba *cap, const struct pqos_cpuinfo *cpu)
         lcpuid(0x80000020, 0x0, &res);
         if (!(res.ebx & (1 << 1))) {
                 LOG_INFO("CPUID 0x10.0: MBA not supported!\n");
-                free(cap);
                 return PQOS_RETVAL_RESOURCE;
         }
 