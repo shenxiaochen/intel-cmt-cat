shenxiaochen/intel-cmt-cat

commit 1618ce1a3b088d57da51c937096b84711c1f9d1e

lib: Discover SMBA feature using cpuid method
This feature is identified by the CPUID Function 8000_0020_EBX_x0.

CPUID Fn8000_0020_EBX_x0 AMD Bandwidth Enforcement Feature Identifiers (ECX=0)
Bits    Field Name      Description
2       L3SBE           L3 external slow memory bandwidth enforcement

Discover the feature and its details using the CPUID Function 8000_0020.

Signed-off-by: Babu Moger <babu.moger@amd.com>

lib/hw_cap.c
@@ -1211,3 +1211,38 @@ amd_cap_mba_discover(struct pqos_cap_mba *cap, const struct pqos_cpuinfo *cpu)
 
         return ret;
 }
+
+int
+amd_cap_smba_discover(struct pqos_cap_mba *cap, const struct pqos_cpuinfo *cpu)
+{
+        struct cpuid_out res;
+
+        UNUSED_PARAM(cpu);
+        ASSERT(cap != NULL);
+
+        memset(cap, 0, sizeof(*cap));
+        cap->mem_size = sizeof(*cap);
+        cap->ctrl = -1;
+        cap->ctrl_on = 0;
+
+        /**
+         * Run CPUID.0x80000020.0 to check
+         * for SMBA allocation capability (bit 2 of ebx)
+         */
+        lcpuid(0x80000020, 0x0, &res);
+        if (!(res.ebx & (1 << 2))) {
+                LOG_INFO("CPUID.0x80000008.0: SMBA not supported\n");
+                free(cap);
+                return PQOS_RETVAL_RESOURCE;
+        }
+
+        lcpuid(0x80000020, 1, &res);
+
+        cap->num_classes = (res.edx & 0xffff) + 1;
+
+        /* AMD does not support throttle_max and is_linear. Set it to 0 */
+        cap->throttle_max = 0;
+        cap->is_linear = 0;
+
+        return PQOS_RETVAL_OK;
+}

lib/cap.c
@@ -295,6 +295,48 @@ cap_mba_discover(struct pqos_cap_mba **r_cap,
 }
 
 /**
+ * @brief Discovers support of SMBA
+ *
+ * @param[out] r_cap place to store SMBA capabilities structure
+ * @param[in] cpu detected cpu topology
+ * @param[in] iface Selected interface
+ *
+ * @return Operation status
+ * @retval PQOS_RETVAL_OK success
+ */
+static int
+cap_smba_discover(struct pqos_cap_mba **r_cap,
+                  const struct pqos_cpuinfo *cpu,
+                  const enum pqos_interface iface)
+{
+        struct pqos_cap_mba *cap = NULL;
+        int ret = PQOS_RETVAL_OK;
+
+        cap = (struct pqos_cap_mba *)malloc(sizeof(*cap));
+        if (cap == NULL)
+                return PQOS_RETVAL_RESOURCE;
+
+        switch (iface) {
+        case PQOS_INTER_MSR:
+                if (cpu->vendor == PQOS_VENDOR_AMD)
+                        ret = amd_cap_smba_discover(cap, cpu);
+                else
+                        ret = PQOS_RETVAL_RESOURCE;
+                break;
+        default:
+                ret = PQOS_RETVAL_RESOURCE;
+                break;
+        }
+
+        if (ret == PQOS_RETVAL_OK)
+                *r_cap = cap;
+        else
+                free(cap);
+
+        return ret;
+}
+
+/**
  * @brief Runs detection of platform monitoring and allocation capabilities
  *
  * @param p_cap place to store allocated capabilities structure
@@ -313,6 +355,7 @@ discover_capabilities(struct pqos_cap **p_cap,
         struct pqos_cap_l3ca *det_l3ca = NULL;
         struct pqos_cap_l2ca *det_l2ca = NULL;
         struct pqos_cap_mba *det_mba = NULL;
+        struct pqos_cap_mba *det_smba = NULL;
         struct pqos_cap *_cap = NULL;
         unsigned sz = 0;
         int ret = PQOS_RETVAL_RESOURCE;
@@ -420,6 +463,29 @@ discover_capabilities(struct pqos_cap **p_cap,
                 goto error_exit;
         }
 
+        /**
+         * Slow Memory bandwidth allocation init
+         */
+        ret = cap_smba_discover(&det_smba, cpu, inter);
+        switch (ret) {
+        case PQOS_RETVAL_OK:
+                LOG_INFO("SMBA capability detected\n");
+                LOG_INFO("SMBA details: "
+                         "#COS=%u, %slinear, max=%u, step=%u\n",
+                         det_smba->num_classes,
+                         det_smba->is_linear ? "" : "non-",
+                         det_smba->throttle_max, det_smba->throttle_step);
+                sz += sizeof(struct pqos_capability);
+                break;
+        case PQOS_RETVAL_RESOURCE:
+                LOG_INFO("SMBA capability not detected\n");
+                break;
+        default:
+                LOG_ERROR("Fatal error encounter in SMBA discovery!\n");
+                ret = PQOS_RETVAL_ERROR;
+                goto error_exit;
+        }
+
         if (sz == 0) {
                 LOG_ERROR("No Platform QoS capability discovered\n");
                 ret = PQOS_RETVAL_ERROR;
@@ -476,6 +542,13 @@ discover_capabilities(struct pqos_cap **p_cap,
                                 goto error_exit;
                 }
 #endif
+        }
+
+        if (det_smba != NULL) {
+                _cap->capabilities[_cap->num_cap].type = PQOS_CAP_TYPE_SMBA;
+                _cap->capabilities[_cap->num_cap].u.smba = det_smba;
+                _cap->num_cap++;
+                ret = PQOS_RETVAL_OK;
         }
 
         (*p_cap) = _cap;

lib/hw_cap.h
@@ -158,6 +158,18 @@ PQOS_LOCAL int hw_cap_mba_discover(struct pqos_cap_mba *cap,
  */
 PQOS_LOCAL int amd_cap_mba_discover(struct pqos_cap_mba *cap,
                                     const struct pqos_cpuinfo *cpu);
+/**
+ * @brief Discovers SMBA support for AMD
+ *
+ * @param cap place to store SMBA capabilities
+ * @param cpu detected cpu topology
+ *
+ * @return Operation status
+ * @retval PQOS_RETVAL_OK success
+ * @retval PQOS_RETVAL_RESOURCE if not supported
+ */
+PQOS_LOCAL int amd_cap_smba_discover(struct pqos_cap_mba *cap,
+                                     const struct pqos_cpuinfo *cpu);
 
 #ifdef __cplusplus
 }

commit 3a0c1cd1b686326fdc486668b4b7281cd39d11e1

lib: Fix inconsistent log messages and comments
The log messages and comments are inconsistent with the CPUID code in
functions amd_cap_mba_discover() and amd_cap_smba_discover().

Fix the typos in the log messages and comments.

Fixes: 1618ce1a3b08 ("lib: Discover SMBA feature using cpuid method")
Fixes: 20f07095f4e0 ("lib: moved HW AMD detection of MBA to hw_cap module")
Fixes: 72beceac50df ("lib: Introduce new function discover_alloc_mba_amd for AMD")
Signed-off-by: Xiaochen Shen <shenxiaochen@open-hieco.net>

lib/hw_cap.c
@@ -1306,11 +1306,11 @@ amd_cap_mba_discover(struct pqos_cap_mba *cap, const struct pqos_cpuinfo *cpu)
         }
 
         /**
-         * We can go to CPUID.0x10.0 to obtain more info
+         * We can go to CPUID.0x80000020.0 to obtain more info
          */
         lcpuid(0x80000020, 0x0, &res);
         if (!(res.ebx & (1 << 1))) {
-                LOG_INFO("CPUID 0x10.0: MBA not supported!\n");
+                LOG_INFO("CPUID.0x80000020.0: MBA not supported!\n");
                 return PQOS_RETVAL_RESOURCE;
         }
 
@@ -1344,7 +1344,7 @@ amd_cap_smba_discover(struct pqos_cap_mba *cap, const struct pqos_cpuinfo *cpu)
          */
         lcpuid(0x80000020, 0x0, &res);
         if (!(res.ebx & (1 << 2))) {
-                LOG_INFO("CPUID.0x80000008.0: SMBA not supported\n");
+                LOG_INFO("CPUID.0x80000020.0: SMBA not supported\n");
                 return PQOS_RETVAL_RESOURCE;
         }
 